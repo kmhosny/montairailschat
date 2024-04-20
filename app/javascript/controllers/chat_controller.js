590cacc779d5c93d657e8fba4ebd10eca2c0ea95
Karim Hosny <kmhosny@users.noreply.github.com>
2024-04-21 00:42:29 +0200

    ui fixes

--- app/javascript/controllers/chat_controller.js
+++ app/javascript/controllers/chat_controller.js
@@ -6,6 +6,7 @@ export default class extends Controller {
   async sendMessage(event) {
     event.preventDefault()
     document.getElementById("loading").removeAttribute("hidden")
+    document.getElementById("no_chat_history").setAttribute("hidden", true)
     const message = document.getElementById("message").value
     const response = await post("/chat",{
       body: JSON.stringify({ message: message }),
@@ -15,8 +16,6 @@ export default class extends Controller {
     if (response.ok) {
       document.getElementById("message").value = ""
       document.getElementById("loading").setAttribute("hidden", true)
-      const messages = document.querySelectorAll("[data-message-selector]");
-      messages.scrollTop = messages[message.length-1].scrollHeight;
     }
   }
 }
