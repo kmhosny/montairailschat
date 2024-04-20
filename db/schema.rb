5b1a88dd7b55a8a4fd7fd022375fdde56bff2625
Karim Hosny <kmhosny@users.noreply.github.com>
2024-04-20 22:58:01 +0200

    use postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_04_20_124610) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "chat_histories", force: :cascade do |t|
     t.text "message", null: false
     t.integer "message_type", default: 0
