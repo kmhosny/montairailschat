fdc170500ab473bb414b6b99cdb529ba3ce8709b
Karim Hosny <kmhosny@users.noreply.github.com>
2024-04-21 02:02:13 +0200

    remove unnecessary logging

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,12 @@ class SessionsController < ApplicationController
 
   def new
     user = User.find_by(email: params[:email])
-    Rails.logger.info("PARAMS #{params.inspect}")
     if user&.authenticate(params[:password])
       Rails.logger.info("User #{user.id} logged in")
       cookies.encrypted[:user_id] = user.id
       redirect_to user_path(user)
     else
-      Rails.logger.info("Failed login attempt for #{params[:email]}")
+      Rails.logger.error("Failed login attempt for #{params[:email]}")
       render :show
     end
   end
